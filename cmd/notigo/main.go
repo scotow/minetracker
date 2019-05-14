ee6cd2062620014f2e125a233b90611b63dc7bb2
Scotow <contact@scotow.com>
2019-05-14 20:32:30 +0200

    Update notigo main to allow specific entity

--- cmd/notigo/main.go
+++ cmd/notigo/main.go
@@ -12,13 +12,15 @@ import (
 )
 
 var (
-	flagHostname  = flag.String("h", "", "mcrcon server hostname")
-	flagPort      = flag.Int("p", 0, "mcrcon server port")
-	flagPassword  = flag.String("P", "", "mcrcon server password (optional)")
-	flagInterval  = flag.Duration("i", time.Second*30, "checking interval")
-	flagWait      = flag.Duration("w", time.Hour, "waiting interval")
-	flagSelf      = flag.String("s", "", "don't notify if this player is online or concerned")
-	flagNotigoKey = flag.String("k", "", "notigo key")
+	flagHostname   = flag.String("h", "", "mcrcon server hostname")
+	flagPort       = flag.Int("p", 0, "mcrcon server port")
+	flagPassword   = flag.String("P", "", "mcrcon server password (optional)")
+	flagInterval   = flag.Duration("i", time.Second*30, "checking interval")
+	flagWait       = flag.Duration("w", time.Hour, "waiting interval")
+	flagSelf       = flag.String("s", "", "don't notify if this player is online or concerned")
+	flagEntityId   = flag.String("e", "", "entity id")
+	flagEntityName = flag.String("E", "", "entity name")
+	flagNotigoKey  = flag.String("k", "", "notigo key")
 )
 
 func main() {
@@ -30,11 +32,13 @@ func main() {
 	}
 
 	report := make(chan error)
-
 	server := NewServer(*flagHostname, *flagPort, *flagPassword, report)
 	notifier := NewNotigoNotifier(*flagNotigoKey, "Skyblock")
+
 	_ = server.Add(NewConnectionTracker(*flagSelf, *flagInterval), notifier)
-	_ = server.Add(NewEntityTracker("wandering_trader", "Wandering Trader", *flagInterval, *flagWait), notifier)
+	if *flagEntityId != "" && *flagEntityName != "" && *flagWait != 0 {
+		_ = server.Add(NewEntityTracker(*flagEntityId, *flagEntityName, *flagInterval, *flagWait), notifier)
+	}
 
 	err := <-report
 	checkError(err)

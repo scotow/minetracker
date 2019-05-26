95bf24b6fcc25613f3a9bb9ff837f829dd4cf1c7
Scotow <contact@scotow.com>
2019-05-26 19:37:35 +0200

    Upgrade entity tracker security

--- tracker/entity.go
+++ tracker/entity.go
@@ -1,9 +1,14 @@
 package tracker
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 	"time"
+)
+
+var (
+	ErrInvalidTestCommand = errors.New("invalid command result for entity testing")
 )
 
 func NewEntityTracker(id, name string, interval time.Duration) *EntityTracker {
@@ -30,15 +35,22 @@ func (et *EntityTracker) Wait() time.Duration {
 }
 
 func (et *EntityTracker) Track(result string) (bool, string, error) {
-	if len(strings.TrimSpace(result)) > 0 {
-		if et.wasPresent {
-			return false, "", nil
-		}
+	trimmed := strings.TrimSpace(result)
 
-		et.wasPresent = true
-		return true, fmt.Sprintf("%s has spawned!", et.name), nil
+	if len(trimmed) == 0 {
+		et.wasPresent = false
+		return false, "", nil
 	}
 
-	et.wasPresent = false
-	return false, "", nil
+	n, err := fmt.Sscanf(trimmed, "The time is %d.", new(int))
+	if n != 1 || err != nil {
+		return false, "", ErrInvalidTestCommand
+	}
+
+	if et.wasPresent {
+		return false, "", nil
+	}
+
+	et.wasPresent = true
+	return true, fmt.Sprintf("%s has spawned!", et.name), nil
 }

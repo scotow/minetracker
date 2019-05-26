263182ae7ec45edd0bf7b53d15f00300707f08ed
Scotow <contact@scotow.com>
2019-05-26 19:32:29 +0200

    Add Discord notifier app (with Notigo)

---
+++ cmd/notigo-discord/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	. "github.com/scotow/minetracker"
+	. "github.com/scotow/minetracker/notifier"
+	. "github.com/scotow/minetracker/runner"
+	. "github.com/scotow/minetracker/tracker"
+)
+
+var (
+	flagHostname = flag.String("h", "127.0.0.1", "mcrcon server hostname")
+	flagPort     = flag.Int("p", 25575, "mcrcon server port")
+	flagPassword = flag.String("P", "", "mcrcon server password (optional)")
+	flagTitle    = flag.String("t", "Minecraft", "notigo notification title")
+
+	flagConnInterval  = flag.Duration("i", time.Second*30, "checking interval for connections")
+	flagConnSelf      = flag.String("s", "", "don't notify if this player is online or concerned")
+	flagConnNotigoKey = flag.String("n", "", "notigo key(s) for connections")
+
+	flagEntityInterval       = flag.Duration("I", time.Second*30, "checking interval for entity")
+	flagEntityId             = flag.String("e", "", "entity id")
+	flagEntityName           = flag.String("E", "", "entity name")
+	flagEntityNotigoKey      = flag.String("N", "", "notigo key(s) for entity")
+	flagEntityDiscordKey     = flag.String("d", "", "discord key for entity")
+	flagEntityDiscordChannel = flag.String("c", "", "discord channel id for entity")
+)
+
+func main() {
+	flag.Parse()
+
+	if *flagHostname == "" || *flagPort <= 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	report := make(chan error)
+	cred := Credentials{Hostname: *flagHostname, Port: *flagPort, Password: *flagPassword}
+	runner, err := NewDirectRunner(cred)
+	if err != nil {
+		checkError(err)
+	}
+	server := NewServer(runner, report)
+
+	ct := NewConnectionTracker(*flagConnSelf, *flagConnInterval)
+	et := NewEntityTracker(*flagEntityId, *flagEntityName, *flagEntityInterval)
+
+	hasTracker := false
+	if cnn := connectionsNotigoNotifiers(); cnn != nil {
+		hasTracker = true
+		_ = server.Add(ct, cnn)
+	}
+
+	if enn := entityNotigoNotifiers(); enn != nil {
+		hasTracker = true
+		_ = server.Add(et, enn)
+	}
+	if edn := entityDiscordNotifier(); edn != nil {
+		hasTracker = true
+		_ = server.Add(et, edn)
+	}
+
+	if !hasTracker {
+		fmt.Println("No tracker initiated.")
+		os.Exit(1)
+	}
+
+	checkError(<-report)
+}
+
+func connectionsNotigoNotifiers() Notifier {
+	if *flagConnInterval <= 0 || *flagConnNotigoKey == "" {
+		return nil
+	}
+
+	return parseNotigoKeys(*flagConnNotigoKey)
+}
+
+func entityNotigoNotifiers() Notifier {
+	if *flagEntityInterval <= 0 || *flagEntityId == "" || *flagEntityName == "" || *flagEntityNotigoKey == "" {
+		return nil
+	}
+
+	return parseNotigoKeys(*flagEntityNotigoKey)
+}
+
+func entityDiscordNotifier() Notifier {
+	if *flagEntityInterval <= 0 || *flagEntityId == "" || *flagEntityName == "" || *flagEntityDiscordKey == "" || *flagEntityDiscordChannel == "" {
+		return nil
+	}
+
+	dn, err := NewDiscordNotifier(*flagEntityDiscordKey, *flagEntityDiscordChannel)
+	checkError(err)
+
+	return dn
+}
+
+func parseNotigoKeys(data string) *MultiNotifier {
+	keys := strings.Split(data, ",")
+	notifiers := make([]Notifier, len(keys))
+	for i, v := range keys {
+		notifiers[i] = NewNotigoNotifier(v, *flagTitle)
+	}
+
+	return NewMultiNotifier(notifiers...)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
